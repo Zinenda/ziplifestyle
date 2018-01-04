0368433f581acd0525358e5701cb0fbec56b62db
Ivo Zinenda <izinenda@gmail.com>
2018-01-04 10:56:33 +0200

    products display

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.tooltipped').tooltip();
+    $('.materialboxed').materialbox();
+    
+    var $container = $('#masonry-grid');
+    // initialize
+    $container.masonry({
+      columnWidth: '.col',
+      itemSelector: '.col',
+    });
 });
